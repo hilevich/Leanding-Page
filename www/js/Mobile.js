1a75e2961d4d447187ccb89ccc29191062ffc7a0
yura <yura2007.31@gmail.com>
2016-11-18 16:55:06 +0200

    Add files via upload

---
+++ www/js/Mobile.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShowSkladAvuto(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+$(document).ready(function(){
+    PopUpHide2();
+});
+function PopUpShowSklad(){
+    $("#popup2").show();
+}
+function PopUpHide2(){
+    $("#popup2").hide();
+}
+$(document).ready(function(){
+    PopUpHide3();
+});
+function PopUpShowSkladRoz(){
+    $("#popup3").show();
+}
+function PopUpHide3(){
+    $("#popup3").hide();
+}
+$(document).ready(function(){
+    PopUpHide4();
+});
+function PopUpShowSkladAthers(){
+    $("#popup4").show();
+}
+function PopUpHide4(){
+    $("#popup4").hide();
+}
+$(document).ready(function(){
+   PopUpHidePomochClose();
+});
+function PopUpShowPomoch(){
+    $("#pomoch").show();
+}
+function PopUpHidePomochClose(){
+    $("#pomoch").hide();
+}
---
+++ www/mail.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$recepient = "busk.yura@gmail.com";
+$sitename = "Компресор захід";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$text = trim($_POST["text"]);
+$email = trim($_POST["email"]);
+$message = "Имя: $name \nТелефон:  $phone\nТекст: $email \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
