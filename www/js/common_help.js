8a06eeedbac6959c0ab7a52ae19c653a6d9b4bf0
yura <yura2007.31@gmail.com>
2016-12-13 14:27:00 +0200

    Add files via upload

---
+++ www/js/common_help.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+
+	$("#form_help").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Дякуємо за заявку. Незабаром ми зв'яжемося з вами");
+			$("#form_help").trigger("reset");
+
+			$(document).ready(function(){
+  			 PopUpHidePomochClose();
+					});
+
+			function PopUpHidePomochClose(){
+    				$("#pomoch").hide();
+			}
+
+		});
+		return false;
+	});
+
+});
